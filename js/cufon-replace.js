0e7e8ef33305637020ca916ae0376737f489c684
diego <daherrerac@gmail.com>
2018-05-25 07:22:55 -0500

    segunda descarga
    segunda tanda de archivos

---
+++ js/cufon-replace.js
@@ -0,0 +1,4 @@
+Cufon.replace('h3', { fontFamily: 'Myriad Pro Light' });
+Cufon.replace('h4', { fontFamily: 'Myriad Pro Light' });
+Cufon.replace('h1', { fontFamily: 'Myriad Pro Light' });
+Cufon.replace('h2', { fontFamily: 'Myriad Pro Light' });
